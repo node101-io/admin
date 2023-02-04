c9a895622791c242a05cfd2a4a90582026897dde
Yunus Gürlek <yunusgurlek@Yunus-MacBook-Pro.local>
2023-02-04 12:55:37 +0100

    Update toURLString utility.

--- utils/toURLString.js
+++ utils/toURLString.js
@@ -1,6 +1,6 @@
 module.exports = str => {
-  if (isNaN(str.toString()))
+  if (typeof str != 'string' && isNaN(str.toString()))
     return '';
 
-  return str.toString().split(' ').map(each => encodeURIComponent(each.trim()).trim()).filter(each => each.length).join('-')
+  return str.toString().split(' ').map(each => encodeURIComponent(each.trim()).toLowerCase().trim()).filter(each => each.length).join('-');
 }
