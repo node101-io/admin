0ef61108f99320be0584fd20635f703fac986550
Necip <sagiroglun22@itu.edu.tr>
2023-06-15 00:29:40 +0300

    /mainnets get added.

---
+++ controllers/api/mainnets/get.js
@@ -0,0 +1,29 @@
+const Guide = require('../../../models/guide/Guide');
+
+module.exports = (req, res) => {
+  req.query.is_deleted = false;
+  req.query.is_mainnet = true;
+
+  Guide.findGuideCountByFilters(req.query, (err, count) => {
+    if (err) return res.json({
+      success: false,
+      error: err
+    });
+
+    Guide.findGuidesByFilters(req.query, (err, data) => {
+      if (err) return res.json({
+        success: false,
+        error: err
+      });
+
+      return res.json({
+        success: true,
+        testnets: data.guides,
+        count,
+        limit: data.limit,
+        page: data.page,
+        search: data.search
+      });
+    });
+  });
+}
