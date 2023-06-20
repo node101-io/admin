b68ec87f1a0b405cd5c576dc4a9fa2cf6dd17c8a
Necip <sagiroglun22@itu.edu.tr>
2023-06-20 16:55:19 +0300

    Klein name change and version controller update.

--- controllers/wizard/get.js
+++ controllers/wizard/get.js
@@ -11,11 +11,11 @@ module.exports = (req, res) => {
     let latest_version = null;
 
     try {
-      const response = await fetch('https://api.github.com/repos/node101-io/node-wizard/tags');
+      const response = await fetch('https://api.github.com/repos/node101-io/klein/tags');
       const tags = await response.json();
-      latest_version = tags[0].name;
+      latest_version = tags[0].name.replace('v', '');
 
-      const latestJsonResponse = await fetch(`https://github.com/node101-io/node-wizard/releases/download/${latest_version}/latest.json`);
+      const latestJsonResponse = await fetch(`https://github.com/node101-io/klein/releases/download/v${latest_version}/latest.json`);
       github_data = await latestJsonResponse.json();
     } catch (err) {
       console.error(err);
@@ -24,7 +24,7 @@ module.exports = (req, res) => {
 
     return res.render('wizard/edit', {
       page: 'wizard/edit',
-      title: res.__('Edit Wizard App Data'),
+      title: res.__('Edit Klein Data'),
       includes: {
         external: {
           css: ['confirm', 'create', 'form', 'formPopUp', 'general', 'header', 'items', 'navbar', 'navigation', 'text'],
